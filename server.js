3d1f42ae24ecbb7653c007d506667a2e4da84b64
EsaKris <esachris00@gmail.com>
2024-08-03 22:51:09 +0100

    new file:   Procfile 	modified:   server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set up the SQLite database
 const db = new sqlite3.Database(':memory:'); // Use ':memory:' for in-memory database or provide a path for a file-based database
